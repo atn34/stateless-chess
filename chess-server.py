e1ae96c5dbce6e4339b7e521900f372c5f1e9e66
Andrew Noyes <anoyes34@gmail.com>
2016-01-14 00:14:34 -0800

    Add hmac so that only legal board states are possible

--- chess-server.py
+++ chess-server.py
@@ -1,14 +1,23 @@
 """Chess server.
-Usage: chess-server.py [--debug] [--port PORT]
+Usage: chess-server.py [--debug] [--port PORT] [--secret SECRET]
 
---port PORT  [default: 8080]
+--port PORT      [default: 8080]
 --debug
+--secret SECRET  [default: secret]
 """
+import binascii
 import bottle
 import chess
+import hashlib
+import hmac
+import os
 import urllib
 
 app = bottle.Bottle()
+
+secret = 'secret'
+
+UUID_LENGTH = 16
 
 
 def compressed_available(path):
@@ -47,34 +56,53 @@ def index():
 </html>
 '''
 
-def move_generator(board):
+
+def trusted_digest(*args):
+    h = hmac.new(secret)
+    for arg in args:
+        h.update(str(arg))
+    return h.hexdigest()
+
+
+def move_generator(board, game_uuid):
     moves = []
     for move in sorted(board.legal_moves, key=lambda x: x.uci()):
         board.push(move)
-        moves.append((move.uci(), '/game/' + urllib.quote(board.fen())))
+        serial_game = board.fen()
+        moves.append((move.uci(), '/' + '/'.join([
+            'game',
+            game_uuid,
+            trusted_digest(
+                game_uuid,
+                serial_game),
+            urllib.quote(serial_game)])))
         board.pop()
     return moves
 
 
 @app.route('/game')
 @app.route('/game/')
-@app.route('/game/<serial_game:path>')
+@app.route('/game/<game_uuid>/<digest>/<serial_game:path>')
 @bottle.view('game.html')
-def game(serial_game=None):
+def game(game_uuid=None, digest=None, serial_game=None):
     if serial_game is None:
         board = chess.Board()
+        game_uuid = binascii.hexlify(os.urandom(UUID_LENGTH))
         bottle.response.set_header('Cache-Control', 'public, max-age=3600')
     else:
+        if not hmac.compare_digest(trusted_digest(game_uuid, serial_game), digest):
+            raise bottle.HTTPError(404, "Tampered link")
         board = chess.Board(serial_game)
     return dict(
         board=board,
         current_url=bottle.request.url,
-        moves=move_generator(board),
+        moves=move_generator(board, game_uuid),
     )
 
 if __name__ == '__main__':
     from docopt import docopt
     arguments = docopt(__doc__)
+    secret = arguments['--secret']
     if arguments['--debug']:
         app.run(host='localhost', debug=True, reloader=True,
                 port=int(arguments['--port']))
